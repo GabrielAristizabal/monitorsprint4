eef690cbf3d17338411ce8296cac436dc359e163
SebasRodriT <js.rodriguezt1@uniandes.edu.co>
2025-12-02 21:58:21 -0500

    commit

--- Gestor_Pedidos/provesi-orders-mongo/microservices/orders-mongo-service/app/infra/mongo_client.py
+++ Gestor_Pedidos/provesi-orders-mongo/microservices/orders-mongo-service/app/infra/mongo_client.py
@@ -1,8 +1,19 @@
+import os
 from motor.motor_asyncio import AsyncIOMotorClient
-from ..core.config import settings
 
-_client = AsyncIOMotorClient(settings.mongo_uri)
-_db = _client[settings.mongo_db]
+# Leer variables de entorno
+MONGO_URI = os.getenv(
+    "MONGO_URI",
+    "mongodb://ruta_user:RutaPasswordSeguro1!@172.31.64.75:3306/ruta_optima?authSource=ruta_optima",
+)
+MONGO_DB = os.getenv("MONGO_DB", "ruta_optima")
+
+print("⚙️ USANDO MONGO_URI:", MONGO_URI)
+print("⚙️ USANDO MONGO_DB:", MONGO_DB)
+
+_client = AsyncIOMotorClient(MONGO_URI)
+_db = _client[MONGO_DB]
+
 
 def get_db():
     return _db
