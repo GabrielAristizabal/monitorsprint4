60236823e335a21d230a34182452fe953a84d3f3
SebasRodriT <js.rodriguezt1@uniandes.edu.co>
2025-12-02 21:50:25 -0500

    Cambio

--- Gestor_Pedidos/provesi-orders-mongo/microservices/orders-mongo-service/app/core/config.py
+++ Gestor_Pedidos/provesi-orders-mongo/microservices/orders-mongo-service/app/core/config.py
@@ -2,7 +2,7 @@ import os
 from pydantic import BaseModel
 
 class Settings(BaseModel):
-    mongo_uri: str = os.getenv("MONGO_URI", "mongodb://localhost:27017")
+    mongo_uri: str = os.getenv("MONGO_URI", "mongodb://ruta_user:RutaPasswordSeguro1!@172.31.64.75:3306/ruta_optima?authSource=ruta_optima")
     mongo_db: str = os.getenv("MONGO_DB", "ruta_optima")
 
 settings = Settings()
