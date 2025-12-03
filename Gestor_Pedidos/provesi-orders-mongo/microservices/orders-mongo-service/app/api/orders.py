3cded246f1a1e2e0713865385c05e2307e7be56f
SebasRodriT <js.rodriguezt1@uniandes.edu.co>
2025-12-02 21:41:56 -0500

    Cambio

--- Gestor_Pedidos/provesi-orders-mongo/microservices/orders-mongo-service/app/api/orders.py
+++ Gestor_Pedidos/provesi-orders-mongo/microservices/orders-mongo-service/app/api/orders.py
@@ -8,7 +8,7 @@ router = APIRouter(prefix="/orders", tags=["orders"])
 @router.post("", response_model=OrderOut)
 async def create_order(order: OrderCreate):
     try:
-        order_id = await insert_order(order.model_dump())
+        order_id = await insert_order(order)
         return OrderOut(id=order_id, erp_order_id=order.erp_order_id, created_at=datetime.utcnow())
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
